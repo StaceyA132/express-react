6bacf38e6113a07c7c1bbbf03c20396a96222ac1
StaceyA132 <staceyannan28@gmail.com>
2024-05-12 07:27:51 -0400

    import express, define port, define server, send back a response, run server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// import express into our file
+const express = require('express')
+
+// create a new express application
+const app = express()
+
+// define the porrt our server will run on
+const port = 3000
+
+//define our server routes
+app.get('/messsage', (req, res)=> {
+    // something when the server processes this request
+
+    // send back a response to the client
+    res.send("hello from our server")
+})
+
+//run our server to listen a the port we defined
+app.listen(port, ()=> {
+    console.log('Server is running on port ${port}')
+})
